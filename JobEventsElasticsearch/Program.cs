99bbf065e721e9d170eab2c32581d55bcca82fac
Johnny Oshika <johnnyoshika@gmail.com>
2019-05-31 14:23:02 -0700

    Add Elasticsearch bulk insert

--- JobEventsElasticsearch/Program.cs
+++ JobEventsElasticsearch/Program.cs
@@ -1,6 +1,7 @@
-﻿using JobEventsElasticsearch.Data;
+﻿using Dapper;
 using Nest;
 using System;
+using System.Data.SqlClient;
 using System.Linq;
 
 namespace JobEventsElasticsearch
@@ -15,30 +16,41 @@ namespace JobEventsElasticsearch
 
         static void Insert()
         {
-            using (var context = new DataContext())
+            using (var connection = new SqlConnection("Server=.;Database=jobcentre_production;Integrated Security=SSPI;MultipleActiveResultSets=true"))
             {
-                var start = new DateTime(2019, 1, 1);
-                var events = context.ZStatJobEvent
-                    .Where(e => e.StatDate >= start)
-                    .Select(e => new JobEvent
-                    {
-                        Id = e.StatId,
-                        At = e.StatDate,
-                        StatTypeId = e.StatTypeId,
-                        JobId = e.JobId,
-                        EmployerId = e.EmployerId,
-                        Source = e.Source2,
-                        Medium = e.Medium,
-                        Campaign = e.Campaign
-                    }).ToArray();
-            }
+                int offset = 0;
+                int next = 50000;
+                while (true)
+                {
+                    var events = connection.Query<JobEvent>(@"
+                        SELECT e.[StatID] as [Id]
+                                ,e.[StatDate] as [EventAt]
+                                ,e.[StatTypeID]
+                                ,e.[EmployerID]
+	                            ,ep.[CompanyName]
+                                ,e.[JobId]
+	                            ,j.[JobTitle]
+                                ,e.[Source2] as [Source]
+                                ,e.[Medium]
+                                ,e.[Campaign]
+                        FROM [dbo].[z_Stat_JobEvent] as e
+	                        LEFT JOIN [dbo].[EmployerProfileDB39] as ep on ep.EmployerID = e.EmployerID
+	                        LEFT JOIN  [dbo].[JobDB39] as j on j.JobID = e.JobId
+                        WHERE e.[StatTypeID] IN (7,10,18,22) AND e.[StatDate] >= @after
+                        ORDER BY e.[StatID]
+                        OFFSET @offset ROWS
+                        FETCH NEXT @next ROWS ONLY
+                    ", new { after = "2019-01-01", offset, next });
 
-            var client = new ElasticClient(new Uri("http://127.0.0.1:9221"));
-            var response = client.IndexMany(new[]
-            {
-                new Widget { Id = 1, Name = "Car" },
-                new Widget { Id = 2, Name = "Plane" }
-            }, "widgets");
+                    if (events.Count() < 1)
+                        break;
+
+                    var client = new ElasticClient(new Uri("http://127.0.0.1:9221"));
+                    var response = client.IndexMany(events, "jobevents");
+
+                    offset = offset + next;
+                }
+            }
         }
     }
 
@@ -51,10 +63,12 @@ namespace JobEventsElasticsearch
     class JobEvent
     {
         public long Id { get; set; }
-        public DateTime At { get; set; }
+        public DateTime EventAt { get; set; }
         public int StatTypeId { get; set; }
-        public int JobId { get; set; }
         public int EmployerId { get; set; }
+        public string CompanyName { get; set; }
+        public int JobId { get; set; }
+        public string JobTitle { get; set; }
         public string Source { get; set; }
         public string Medium { get; set; }
         public string Campaign { get; set; }
