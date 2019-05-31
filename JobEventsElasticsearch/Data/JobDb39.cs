957da2bcc24c3ff625b34f21dd7c91961b55ace6
Johnny Oshika <johnnyoshika@gmail.com>
2019-05-31 11:41:31 -0700

    Add DataContext

---
+++ JobEventsElasticsearch/Data/JobDb39.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JobEventsElasticsearch.Data
+{
+    public partial class JobDb39
+    {
+        public int JobId { get; set; }
+        public int EmployerId { get; set; }
+        public string JobTitle { get; set; }
+        public int StatusId { get; set; }
+
+        public virtual EmployerProfileDb39 Employer { get; set; }
+    }
+}
